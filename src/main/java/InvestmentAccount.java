91508f15ff3ebafbad54d2fa11c5e9bfe51acc55
Gerrod Mozeik <gerrodmozeik@zipcoders-MacBook-Pro-9.local>
2017-01-17 15:54:52 -0500

    Added enums

---
+++ src/main/java/InvestmentAccount.java
@@ -0,0 +1,5 @@
+/**
+ * Created by gerrodmozeik on 1/17/17.
+ */
+public class InvestmentAccount extends BankAccount {
+}
---
+++ src/main/java/ODPStatus.java
@@ -0,0 +1,6 @@
+/**
+ * Created by gerrodmozeik on 1/17/17.
+ */
+public enum ODPStatus {
+    ON, OFF, AUTOTRANSFER
+}

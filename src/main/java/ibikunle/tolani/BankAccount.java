51cf3badca48f02016540d39aba933e7def33bb0
Tolani Ibikunle <tolaniibikunle@zipcoders-MacBook-Pro-10.local>
2017-01-17 18:12:27 -0500

    first commit

---
+++ src/main/java/ibikunle/tolani/BankAccount.java
@@ -0,0 +1,22 @@
+package ibikunle.tolani;
+
+/**
+ * Created by tolaniibikunle on 1/17/17.
+ */
+public class BankAccount {
+    static double accountNumber = 0;
+    protected enum differentAcountTypes{Checkings,Savings, Investments}
+
+    protected static void createBankAccount(String name,String bankAccountType){}
+
+    protected double setBankAccountNumber() {
+        accountNumber += 1;
+        accountNumber++;
+        return accountNumber;
+    }
+    protected static void setOverdraft(){}
+
+    protected static void setBankAccountStatus(){}
+
+    protected static void setInterestRate(){}
+}

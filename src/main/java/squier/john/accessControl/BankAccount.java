bda0ccc35e24f878f3bb3a16c3d25c2d8b133467
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-17 15:23:25 -0500

    More code and updated .gitignore

--- src/main/java/squier/john/accessControl/BankAccount.java
+++ src/main/java/squier/john/accessControl/BankAccount.java
@@ -17,5 +17,44 @@ public class BankAccount {
     private OverdraftProtection overdraftProtection;
     private ArrayList<BankAccountTransaction> transactionRecord;
 
+    public BankAccount(BankAccountType accountType, double balance, String accountHoldersName, double interestRate,
+                       BankAccountStatus accountStatus, OverdraftProtection overdraftProtection) {
+        this.accountType = accountType;
+        accountNumber = nextAccoutNumber++;
+        this.balance = balance;
+        this.accountHoldersName = accountHoldersName;
+        this.interestRate = interestRate;
+        this.accountStatus = accountStatus;
+        this.overdraftProtection = overdraftProtection;
+        transactionRecord = new ArrayList<BankAccountTransaction>();
+    }
 
+    public Double getBalance() {
+        if ( accountStatus.equals(BankAccountStatus.OFAC_FROZEN) ) {
+            // do nothing probably throw an expection at some point
+            return null;
+        }
+        else {
+            return balance;
+        }
+    }
+
+    public ApprovalStatus updateBalanceWithCreditOrDebit(double amount) {
+        if ( accountStatus.equals(BankAccountStatus.OPEN) ) {
+            if ( amount > 0.0 ) {
+                credit(amount);
+            }
+            else {
+                debit(amount);
+            }
+        }
+    }
+
+    private ApprovalStatus credit(double amount) {
+
+    }
+
+    private ApprovalStatus debit(double amount) {
+
+    }
 }
--- src/main/java/squier/john/accessControl/TransactionType.java
+++ src/main/java/squier/john/accessControl/TransactionType.java
@@ -4,4 +4,4 @@ package squier.john.accessControl;
  * Created by johnsquier on 1/17/17.
  */
 public enum TransactionType {WITHDRAWL, DEPOSIT, TRANSFER, STATUS_CHANGE, NAME_CHANGE, INTEREST_RATE_CHANGE;}
-}
+

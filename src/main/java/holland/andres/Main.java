26ad52c36ac614840294e0e9cbe86ef81caa18c7
Andres Holland <andresholland@zipcoders-MacBook-Pro-7.local>
2017-01-19 08:55:13 -0500

    Add UML

--- src/main/java/holland/andres/Main.java
+++ src/main/java/holland/andres/Main.java
@@ -1,13 +1,6 @@
 package holland.andres;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        Account account1 = new Account("andres holland","checking","enabled",3);
-        Account account2 = new Account("andres holland","checking","enabled",3);
-        System.out.println(account1.getAccountNumber());
-        System.out.println(account2.getAccountNumber());
     }
-
 }
